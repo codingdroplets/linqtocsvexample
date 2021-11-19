8588e455de5b68f149ca35cc4edcc00a6350a71a
Coding Droplets <93096161+codingdroplets@users.noreply.github.com>
2021-11-20 02:48:38 +0530

    Add files via upload

---
+++ Country.cs
@@ -0,0 +1,24 @@
+﻿using LINQtoCSV;
+using System;
+
+namespace CsvDemo
+{
+    [Serializable]
+    public class Country
+    {
+        [CsvColumn(Name = "name", FieldIndex = 1)]
+        public string CountryName { get; set; }
+
+        [CsvColumn(Name = "alpha-2", FieldIndex = 2)]
+        public string Alpha2 { get; set; }
+
+        [CsvColumn(Name = "alpha-3", FieldIndex = 3)]
+        public string Alpha3 { get; set; }
+
+        [CsvColumn(Name = "country-code", FieldIndex = 4)]
+        public string CountryCode { get; set; }
+
+        [CsvColumn(Name = "region", FieldIndex = 5)]
+        public string Region { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using LINQtoCSV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ReadCsvFile();
+            WriteCsvFile();
+            Console.ReadKey();
+        }
+
+        private static void WriteCsvFile()
+        {
+            var countryList = new List<Country>
+            {
+                new Country{ CountryName = "Afghanistan", Alpha2 = "AF", Alpha3 = "AFG", CountryCode = "4", Region = "Asia"},
+                new Country{ CountryName = "Åland Islands", Alpha2 = "AI", Alpha3 = "AIS", CountryCode = "5", Region = "Asia"},
+                new Country{ CountryName = "Albania", Alpha2 = "AB", Alpha3 = "ALB", CountryCode = "6", Region = "Asia"}
+            };
+
+            var csvFileDescription = new CsvFileDescription
+            {
+                FirstLineHasColumnNames = true,
+                SeparatorChar = ','
+            };
+
+            var csvContext = new CsvContext();
+            csvContext.Write(countryList, "country01.csv", csvFileDescription);
+
+            Console.WriteLine("CSV File Created");
+        }
+
+        private static void ReadCsvFile()
+        {
+            var csvFileDescription = new CsvFileDescription
+            {
+                FirstLineHasColumnNames = true,
+                IgnoreUnknownColumns = true,
+                SeparatorChar = ',',
+                UseFieldIndexForReadingData = false
+            };
+
+            var csvContext = new CsvContext();
+            var countries = csvContext.Read<Country>("country.csv", csvFileDescription);
+
+            foreach(var country in countries)
+            {
+                Console.WriteLine($"{country.CountryName} | {country.Alpha3} | {country.CountryCode}");
+            }
+        }
+    }
+}
